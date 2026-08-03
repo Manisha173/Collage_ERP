5d848647505374ebe326fc224ca9c825fc5af168
Gaurav Jaisawal <gj4444gauravjaiswal@gmail.com>
2026-08-03 18:37:21 +0530

    save my design changes

---
+++ College_ERP/Controllers/ParentController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace College_ERP.Controllers
+{
+    public class ParentController : Controller
+    {
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public ActionResult Timetable()
+        {
+            return View();
+        }
+
+        public ActionResult Assignment()
+        {
+            return View();
+        }
+
+        public ActionResult Parentcommunication()
+        {
+            return View();
+        }
+
+        public ActionResult Announcement()
+        {
+            return View();
+        }
+        public ActionResult Result()
+        {
+            return View();
+        }
+        public ActionResult Notice()
+        {
+            return View();
+        }
+
+        public ActionResult ExamSchedule()
+        {
+            return View();
+        }
+
+        public ActionResult Fees()
+        {
+            return View();
+        }
+        public ActionResult Hostel()
+        {
+            return View();
+        }
+        public ActionResult Transport()
+        {
+            return View();
+        }
+        public ActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
